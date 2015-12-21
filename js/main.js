95cd66b98f64888922c59ffb7b2d27e8f5b49f8c
MacBook Owner <macbookowner@MacBooks-MacBook.local>
2015-12-21 18:44:46 -0500

    update home page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,64 @@ $(document).ready(function(){
     account: "Montezuma-Half-Marathon-5K-970747939609605"
   });*/
 
+/* $("").hover(function () {
+	$(this).find("img").animate({
+		"opacity" : 0.6,
+	}); //animate on
+}, function () {
+	$(this).find("img").animate({
+		"opacity" : 1.0, 
+	}); // animate off
+}); // complete hover
+
+$("col-xs-12 col-sm-8 col-md-4").hover(function () {
+	$(this).find("img").animate({
+		"opacity" : 0.6,
+	}); //animate on
+}, function () {
+	$(this).find("img").animate({
+		"opacity" : 1.0, 
+	}); // animate off
+}); // complete hover
+
+$("#image3").hover(function () {
+	$(this).find("img").animate({
+		"opacity" : 0.6,
+	}); //animate on
+}, function () {
+	$(this).find("img").animate({
+		"opacity" : 1.0, 
+	}); // animate off
+}); // complete hover
+
+$("#image4").hover(function () {
+	$(this).find("img").animate({
+		"opacity" : 0.6,
+	}); //animate on
+}, function () {
+	$(this).find("img").animate({
+		"opacity" : 1.0, 
+	}); // animate off
+}); // complete hover
+
+$("#image5").hover(function () {
+	$(this).find("img").animate({
+		"opacity" : 0.6,
+	}); //animate on
+}, function () {
+	$(this).find("img").animate({
+		"opacity" : 1.0, 
+	}); // animate off
+}); // complete hover
+
+$("#image6").hover(function () {
+	$(this).find("img").animate({
+		"opacity" : 0.6,
+	}); //animate on
+}, function () {
+	$(this).find("img").animate({
+		"opacity" : 1.0, 
+	}); // animate off
+}); // complete hover */
+
 }); //ready method end
